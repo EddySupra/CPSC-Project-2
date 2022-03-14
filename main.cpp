1d9e79878517b8e9e3140a58039921bbdec70c66
EddySupra <Eduardo23@csu.fullerton.edu>
2022-03-14 14:51:25 -0700

    Part A done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,30 @@
+4
+#include <bits/stdc++.h>
+    int
+    main()
+{
+    // Get user input
+    int n;
+    std::cout << "Enter a number" << std::endl;
+    std::cin >> n;
 
+    int fib = 0;
+    int F1 = 0;
+    int F2 = 1;
+
+    if (n <= 1)
+    {
+        return n;
+    }
+    else
+    {
+        std::cout << F1 << ", ";
+        for (int i = 1; i < n; i++)
+        {
+            std::cout << F2 << ", ";
+            int next = F1 + F2;
+            F1 = F2;
+            F2 = next;
+        }
+    }
+}
