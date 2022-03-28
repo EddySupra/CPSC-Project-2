49f3a50351e3cb20d4fd76d1278680c7f8c3d327
Ismail Hasan <ismailhasan0132@gmail.com>
2022-03-27 21:26:24 -0700

    updated 1b

--- mainB.cpp
+++ mainB.cpp
@@ -3,38 +3,44 @@
 
 using namespace std;
 
-double nthTermFibbonacci(int n);
+double nthTermFibonacci(int n);
 double npFibbonacci(int p, int n);
 double nextTermFibonnaci(int n);
 int getPositiveInt();
 
 int main() {
 
+    cout << "First 20 terms of the Fibonacci Sequence: " << endl;
+    for(int i = 1; i <= 20; ++i) {
+        cout << i << ": " << nthTermFibonacci(i) << endl;
+    }
+
+    cout << endl;
+
     cout << "Enter a positive integer p." << endl;
     int p = getPositiveInt();
 
-    cout << "Enter the nth value of the Fibbonacci Sequence you want to compute" << endl;
+    cout << "Enter the nth value of the Fibonacci Sequence you want to compute" << endl;
     int n = getPositiveInt();
 
     cout << endl << endl;
 
-    cout << "Fibonacci Sequence using Equation 4: " << endl << endl;
-    
+    cout << n <<"th term of Fibonacci Sequence using Equation 4 with p value of " << p << ": " << endl;
+
     cout << npFibbonacci(p, n);
     cout << endl;
     cout << endl;
 
-    cout << "Fibonacci Sequence using Equation 5: " << endl << endl;
-    for(int i = 0; i < 20; ++i) {
-        cout << "Term " << i + 1 << ": " << nextTermFibonnaci(i) << endl;
-    }
+    cout << n <<"th term of Fibonacci Sequence using Equation 5: " << endl;
+    cout << nextTermFibonnaci(n - 1);
+
     cout << endl;
     cout << endl;
-    cout << "F4 divided by F3 is " << nthTermFibbonacci(4) / nthTermFibbonacci(3) << endl;
-    cout << "F31 divided by F30 is " << nthTermFibbonacci(31) / nthTermFibbonacci(30) << endl;
+    cout << "F4 divided by F3 is " << nthTermFibonacci(4) / nthTermFibonacci(3) << endl;
+    cout << "F31 divided by F30 is " << nthTermFibonacci(31) / nthTermFibonacci(30) << endl;
 }
 
-double nthTermFibbonacci(int n) {
+double nthTermFibonacci(int n) {
 
     double firstTerm = pow(1 + sqrt(5), n);
     double secondTerm = pow(1 - sqrt(5), n);
@@ -44,20 +50,20 @@ double nthTermFibbonacci(int n) {
 }
 
 double npFibbonacci(int p, int n) {
-    double firstTerm = nthTermFibbonacci(p);
+    double firstTerm = nthTermFibonacci(p);
     double secondTerm = pow(1.61803, n-p);
     return firstTerm * secondTerm;
 }
 
 double nextTermFibonnaci(int n) {
-    return nthTermFibbonacci(n) * 1.61803;
+    return nthTermFibonacci(n) * 1.61803;
 }
 
 int getPositiveInt() {
     float temp;
     cin >> temp;
     if(int(temp) != temp || temp < 0) {
-        cout << "Input was float or negative please try again. " <<endl;
+        cout << "Input was float or negative please try again. " << endl;
         return getPositiveInt();
     }
     return int(temp);
